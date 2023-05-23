e684a9cb5e719c4a70bb4d4aefa7b6715d817be2
Piztle17 <mustaphaabiola17@gmail.com>
2023-05-23 16:10:55 -0500

    firsst commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const menuicon = document.querySelector(".menu-icon");
+const nav = document.querySelector(".h-menu");
+
+menuicon.addEventListener("click", ()=>{
+    nav.classList.toggle("openme");
+})
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
